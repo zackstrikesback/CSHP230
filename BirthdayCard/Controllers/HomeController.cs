4ca695bb09a7aa7c1961f5610bab96800b51c057
Zachary Chung <Zachary Chung>
2017-05-08 08:41:35 -0700

    BirthdayCard

---
+++ BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardForm(Models.Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Sent", card);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
